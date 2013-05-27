2f8441477569ed4dc86a5dffd7a0571c6f4b756d
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-27 09:37:53 -0700

    Initial commit/gemfile created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterResque::Application.config.secret_token = '1c0d02b5529c96b05b40d9a78ddd741f3d52d803b42be4596d69f00e2f48778a5c79983098ce031c915c1638d2f4fb498d03a721132b65224e17a67f03424c49'
